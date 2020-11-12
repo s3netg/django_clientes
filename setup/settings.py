feedd6a0db960300cc6370a32a4282bf93ab53e0
s3netg <s3netg@gmail.com>
2020-11-11 23:07:52 -0300

    projeto

--- setup/settings.py
+++ setup/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -128,4 +128,3 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
-django_heroku.settings(locals())
